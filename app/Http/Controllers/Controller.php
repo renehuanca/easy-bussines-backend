23e2d11fbab8405edf3de4844e022f7af41e175d
Rene Huanca <renehuanca999@gmail.com>
2025-04-04 11:14:49 -0400

    feat: integrate L5 Swagger for API documentation and add configuration files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,32 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Easy Business API Documentation",
+ *     description="API documentation for Easy Business application",
+ *     @OA\Contact(
+ *         email="renehuanca999@gmail.com"
+ *     )
+ * )
+ * 
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ * 
+ * @OA\PathItem(
+ *     path="/api"
+ * )
+ */
 abstract class Controller
 {
     //
